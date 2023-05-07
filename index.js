e45d9b3322cf490b336bd7f211c748a5a67bc6c8
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-05-07 16:40:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+const { Client, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const axios = require('axios');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const rest = new REST({ version: '9' }).setToken(`${config.token}`);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand() || interaction.commandName !== 'node') return;
+
+    const mention = interaction.options.getMentionable('node');
+    if (!mention) {
+        await interaction.reply('Please mention a node.');
+        return;
+    }
+    const nodeName = mention.name;
+
+    const url = `https://${url}/api/application/nodes/${nodeName}/utilization`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${api}`,
+                'Content-Type': 'application/json',
+                'Accept': 'Application/vnd.pterodactyl.v1+json',
+            },
+        });
+
+        const data = response.data;
+
+        const embed = {
+            title: `Node ${nodeName} Stats`,
+            description: `CPU: ${data.cpu.toFixed(2)}%\nRAM: ${data.memory.current}/${data.memory.limit} MB (${((data.memory.current / data.memory.limit) * 100).toFixed(2)}%)\nDisk: ${data.disk.current}/${data.disk.limit} MB (${((data.disk.current / data.disk.limit) * 100).toFixed(2)}%)`,
+            color: 0x00ff00,
+        };
+
+        await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+        console.error(error);
+        await interaction.reply('An error occurred while fetching node stats.');
+    }
+});
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands(`${config.appid}`, `${config.gid}`),
+            { body: [
+                {
+                    name: 'node',
+                    description: 'Get stats for a Pterodactyl node.',
+                    options: [
+                        {
+                            name: 'node',
+                            description: 'The Pterodactyl node to get stats for.',
+                            type: 'MENTIONABLE',
+                            required: true,
+                        },
+                    ],
+                },
+            ]},
+        );
+
+        console.log('Successfully registered slash commands.');
+    } catch (error) {
+        console.error(error);
+    }
+
+    await client.login(`${config.token}`);
+})();
